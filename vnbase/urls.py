2295996f925bcd79e2cc153c96afd14d0fd2c9be
Moon <moon@163.com>
2024-11-25 19:02:20 +0800

    compose.yml

--- vnbase/urls.py
+++ vnbase/urls.py
@@ -7,8 +7,8 @@ app_name = "vnbase"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("mvn/<str:param>/", views.mvn, name="mvn"),
-    path("zfbvn/<str:param>/", views.zfbvn, name="zfbvn"),
+    path("mvn/<str:param>", views.mvn, name="mvn"),
+    path("zfbvn/<str:param>", views.zfbvn, name="zfbvn"),
     #  'api/zfbwebvn/ZfbVnPriceList',
     path("zfbwebvn/ZfbVnPriceList", api.ZfbVnPriceList),
     path("zfbwebvn/ZfbVnOrderCreate", api.ZfbVnOrderCreate),
