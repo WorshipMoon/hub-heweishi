ef46e0c1a2ee43a0a9f8758b531364117505ae6d
WorshipMoon <736555030@qq.com>
2024-11-25 12:02:26 +0000

    Deploying to self-dist from @ WorshipMoon/hub-heweishi@48076687857448dadc465aec6023215c4c4aaa15 🚀

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ mv .env.example .env && \
 sudo chown -R 1002:1002 .env && \
 sudo chmod 755 .env && \
 sudo chown -R 1002:1002 gunicorn.conf.py && \
-sudo chmod 755 gunicorn.conf.py && \
+sudo chmod 755 gunicorn.conf.py
 
 # Check if all commands completed successfully
 if [ $? -eq 0 ]; then
